972261eaa8ebf2c0a606c3e1694aa0061596ca7c
Varun Nair <varunsnair1997@gmail.com>
2024-04-18 02:25:38 +0530

    ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
-import { Button, Dimensions, FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Alert, Button, Dimensions, FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import Chart from './components/Chart';
-import { Icon } from '@rneui/base';
-import Card from './components/Card';
+import { Icon, color } from '@rneui/base';
 import { ScrollView } from 'react-native';
+import { Linking } from 'react-native';
+import {amazone} from '../../assets/download (1).png'
+
+
+
 
 export default function Home() {
     const [data,setData]=useState(null)
+    const [show,setShow]=useState(false)
 
    
 const [wish, setWish] = useState('Good Morning');
@@ -20,7 +25,26 @@ console.log("WISH",wish)
 console.log("Data",data)
 console.log("check...........",data?.data.recent_links)
 
+
+
 const recentLinks=data?.data.recent_links
+
+
+
+ const currentDate = new Date();
+
+
+  const year = currentDate.getFullYear();
+  const month = currentDate.getMonth() + 1;
+  const day = currentDate.getDate();
+
+ 
+  const formattedDate = `${year}-${month < 10 ? '0' + month : month}-${day < 10 ? '0' + day : day}`;
+
+
+
+
+
 
 
 
@@ -34,8 +58,24 @@ const Click=data?.data?.recent_links?.map((item)=>{
   return item.total_clicks
 })
 
+const handleTop=()=>{
+  setShow(true)
+}
+
+const handleRecent=()=>{
+  setShow(false)
+}
+
+
+const topLinks=data?.data.top_links
+
+
 console.log("Clicksss",Click)
 console.log("RecentLinks",recentLinks)
+console.log("show....",show)
+console.log("Toppppppppp....",data?.data.top_links)
+console.log("testTOP...",topLinks)
+  console.log("Dateeee",formattedDate)
 
   useEffect(() => {
     const intervalId = setInterval(() => {
@@ -90,11 +130,21 @@ console.log("RecentLinks",recentLinks)
       </View>
       <Chart/>
       <View style={styles.first}>
+
         <View style={styles.inner}>
+        <Icon style={styles.iconInner} color="#7B68EE" type='ionicon' name='arrow-up-circle-outline'></Icon>
+        <Text style={styles.innerText}>123</Text>
+         <Text style={styles.innerText1}>Today's clicks</Text>
         </View>
          <View style={styles.inner}>
+         <Icon style={styles.iconInner}  color="#6495ED" type='ionicon' name='location-outline'></Icon>
+        <Text style={styles.innerText}>Bangalure</Text>
+         <Text style={styles.innerText1}>Top Location</Text>
         </View>
          <View style={styles.inner}>
+         <Icon style={styles.iconInner}  color="#DC143C" type='ionicon' name='globe-outline'></Icon>
+        <Text style={styles.innerText}>Instagram</Text>
+         <Text style={styles.innerText1}>Top source</Text>
         </View>
       </View>
 <View style={styles.cover}>
@@ -115,10 +165,48 @@ console.log("RecentLinks",recentLinks)
 
 <View style={styles.second}>
 <View style={styles.link}>
+
+<View style={styles.link1}>
+<TouchableOpacity onPress={handleTop}>
 <Text style={styles.textLink}>Top Links</Text>
-<Text style={styles.secondtext}>Recent Links</Text>
+</TouchableOpacity>
+
+<TouchableOpacity onPress={handleRecent}>
+<Text style={styles.textLink1}>Recent Links</Text>
+</TouchableOpacity>
+</View>
+
 <Icon type='ionicon' name='search-outline'></Icon>
 </View>
+
+
+{show?<View style={styles.dataBox}>
+
+{topLinks?.map((item)=>(
+
+  <View style={styles.box}>
+
+<View style={styles.mapWrap}>
+<Image
+        source={{ uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkkX5L0ubEpO3tsc5wAgQhtAqu6f8Mzr7dwV3JpytgNg&s' }}
+      
+      />
+      <View>
+  <Text style={styles.datatext}>{item.app}</Text>
+  <Text>{formattedDate}</Text>
+  </View>
+  <Text style={styles.click}>{item.total_clicks}</Text>
+ </View>
+ <View style={styles.mapWrap1} >
+<Text style={styles.mapWraplink}  onPress={() => Linking.openURL(item.web_link)}>{item.web_link}</Text>
+
+<Icon type='ionicon' name='copy-outline'></Icon>
+ 
+ </View>
+</View>
+))}
+
+</View>:
 
 
 <View style={styles.dataBox}>
@@ -128,16 +216,54 @@ console.log("RecentLinks",recentLinks)
   <View style={styles.box}>
 
 <View style={styles.mapWrap}>
+<Image
+        source={{ uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkkX5L0ubEpO3tsc5wAgQhtAqu6f8Mzr7dwV3JpytgNg&s' }}
+      
+      />
+      <View>
   <Text style={styles.datatext}>{item.app}</Text>
+  <Text>{formattedDate}</Text>
+  </View>
+
+  <View>
   <Text style={styles.click}>{item.total_clicks}</Text>
+  <Text style={styles.clickText}>Clicks</Text>
+  </View>
  </View>
  <View style={styles.mapWrap1} >
- <Text>{item.web_link}</Text>
- <Icon type='ionicon' name='copy-outline'></Icon>
+<Text style={styles.mapWraplink}  onPress={() => Linking.openURL(item.web_link)}>{item.web_link}</Text>
+
+<Icon type='ionicon' name='copy-outline'></Icon>
+ 
  </View>
 </View>
 ))}
 
+</View>
+ 
+
+}
+
+</View>
+
+<View style={styles.bottom}>
+
+<View style={styles.view}>
+<Icon type='ionicon' name='link-outline'></Icon>
+<Text >View all links</Text>
+</View>
+
+
+<View style={styles.last}>
+
+<View style={styles.lastbox}>
+<Icon type='ionicon' name='logo-whatsapp'></Icon>
+  <Text>Chat</Text>
+  </View>
+  <View style={styles.lastbox1}>
+  <Icon type='ionicon' name='help-circle-outline'></Icon>
+   <Text>Frequently asked question</Text>
+   </View>
 </View>
 
 </View>
@@ -160,14 +286,100 @@ const styles = StyleSheet.create({
   img:{
     width:"100%"
   },
-  box:{
+  link1:{
+    flexDirection:"row",
+    gap:10
+
+  },
+  last:{
+    gap:20,
+    marginTop:50,
+    marginBottom:50,
+    
+  
+   
+  }
+  ,
+
+lastbox:{
+  flexDirection:"row",
+  borderWidth:1.4,
+ borderColor:"#90EE90",
+  borderRadius:10,
+  width:300,
+  height:50,
+  padding:10,
+  gap:20,
+  backgroundColor:"#D0F0C0",
+  
+  
+ 
+
+},
+lastbox1:{
+  flexDirection:"row",
+  borderWidth:1.4,
+  borderRadius:10,
+  width:300,
+  height:50,
+  padding:10,
+  gap:20,
+  borderColor:"#00BFFF",
+  backgroundColor:"#ADD8E6"
+ 
+
+},
+  iconInner:{
+ 
+    color:"red",
+    justifyContent:"left",
+    textAlign:"left",
+    alignItems:"flex-start",
+    marginBottom:20,
+  },
+  view:{
+    borderWidth:.4,
+    borderColor:"grey",
+    width:"90%",
+    textAlign:"center",
+    height:50,
+    alignItems:"center",
+    justifyContent:"center",
+    marginTop:20,
+    flexDirection:"row",
+    gap:10,
+     borderRadius:10,
+  },
+  bottom:{
     borderWidth:2,
-    borderColor:"red",
-    padding:10,
+    justifyContent:"center",
+    alignItems:"center",
+  },
+  clickText:{
+    color:"grey"
+  },
+  innerText1:{
+    fontWeight:"100"
+
+  },
+  innerText:{
+    fontWeight:"800"
+  },
+  box:{
+    // borderWidth:2,
+    // borderColor:"red",
+    backgroundColor:"#fff",
+    borderRadius:10,
+    justifyContent:"space-between",
     height:100,
   },mapWrap:{
+    padding:20,
     flexDirection:"row",
     justifyContent:"space-between"
+  },
+  mapWraplink:{
+    color:"#1877F2",
+    fontSize:15,
   },
   mapWrap1:{
     flexDirection:"row",
@@ -175,11 +387,15 @@ const styles = StyleSheet.create({
     borderStyle:"dotted",
     borderWidth:1,
     borderColor:"#1E90FF",
-    backgroundColor:"#B0E0E6",
+    backgroundColor:"#E1EBEE",
+    padding:10,
+    borderBottomRightRadius:10,
+    borderBottomLeftRadius:10,
   },
 
 
   datatext:{
+    fontSize:18
     
   },
   mapdata:{
@@ -188,7 +404,9 @@ const styles = StyleSheet.create({
     borderWidth:3,
   },
   dataBox:{
-    borderWidth:2,
+
+    padding:25,
+    gap:50
 
   },
   listMap:{
@@ -203,6 +421,7 @@ const styles = StyleSheet.create({
   secondtext:{
     color:"grey",
     fontSize:14,
+  
 
   },
   link:{
@@ -215,12 +434,23 @@ const styles = StyleSheet.create({
   },
   textLink:{
     fontSize:15,
-    borderRadius:20,
+    borderRadius:15,
     padding:10,
-    width:"25%",
+    width:"100%",
     textAlign:"center",
     backgroundColor:"#1E90FF",
     color:"white",
+
+
+  },
+   textLink1:{
+    fontSize:15,
+    borderRadius:15,
+    padding:10,
+    width:"100%",
+    textAlign:"center",
+    backgroundColor:"#fff",
+    color:"grey",
 
 
   },
@@ -234,7 +464,8 @@ const styles = StyleSheet.create({
   
     flexDirection:"row",
     justifyContent:"space-between",
-    padding:10
+    padding:10,
+    
   },
   out:{
     // borderWidth:2,
@@ -250,9 +481,10 @@ const styles = StyleSheet.create({
   inner:{
     width:100,
     height:100,
-    borderWidth:1,
-    borderColor:"red",
-    backgroundColor:"#fff"
+    borderRadius:10,
+    backgroundColor:"#fff",
+    padding:10,
+  
   },
   cover:{
     // borderColor:"red",
